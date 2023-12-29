ff708062fd6420f631044c16b9a6a0a7b5f7aa2f
Noor-Ul-Hassan <93768672+Geeky-Hassan@users.noreply.github.com>
2023-12-29 09:07:01 +0500

    Update app.py

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ parkinsons_model = pickle.load(open('parkinsons_model.sav', 'rb'))
 # sidebar for navigation
 with st.sidebar:
     
-    selected = option_menu('Multiple Disease Prediction System',
+    selected = option_menu('Multiple Disease Prediction System developed by Noor-Ul-Hassan',
                           
                           ['Diabetes Prediction',
                            'Heart Disease Prediction',
